31c3e2dec2f725b32b8d18ebb3b09db52e24d7ca
SAI VIGNESH V B <saivignesh037@gmail.com>
2016-09-01 20:31:04 +0530

    Create findpos.c

---
+++ findpos.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main()
+{
+printf("enter the number:");
+int a;
+scanf("%d",&a);
+if(a==0)
+{
+printf("number s zero");
+}
+else if(a>0)
+{
+printf("positive");
+}
+else
+{
+printf("neg")';
+}
+}
+
